18f1e72bd06ee799b5f785202f9a5e0dbc94f104
lionenergyautomation <automation@lionenergy.com>
2025-07-28 10:30:12 -0600

    needs the grid stuff still. check links

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,58 @@
+const short = document.querySelector("#short");
+const year = document.querySelector("#year")
+
+const today = new Date();
+
+const time = new Intl.DateTimeFormat("en-US", {
+    timeStyle: "medium",
+}).format(today);
+
+short.innerHTML = `Last Modification: <span class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+    {
+        dateStyle: "short"
+    }
+).format(today)}  ${time}</,span>`;
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+const nav = document.querySelector('nav');
+const hamburger_button = document.querySelector(".hamburger");
+
+hamburger_button.addEventListener('click', () => {
+    nav.toggleAttribute("open");
+    hamburger_button.toggleAttribute("open");
+})
+
+
+const locationCard = document.getElementById("location-card");
+
+async function GetLocations() {
+    try {
+        const response = await fetch("data/discover.json");
+        const locations = await response.json();
+        displayLocations(locations);
+    } catch (error) {
+        console.error("Error loading members:", error)
+    }
+}
+
+function displayLocations(locations) {
+    locationCard.innerHTML = "";
+    locations.forEach(location => {
+        const card = document.createElement("div");
+        card.classList.add("location");
+        card.innerHTML = `
+        <h2>${location.name}</h2>
+        <div class="image-wrapper">
+            <img src="images/${location.image}" alt="Logo of ${location.name}">
+        </div>
+        <p>${location.address}</p>
+        <p>${location.description}</p>  
+        <button>Learn More</button>  
+        `;
+        locationCard.appendChild(card);
+    });
+}
+
+GetLocations();

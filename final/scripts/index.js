381e2d66cd87f60d83187157efbc3dad09403cfd
bpace522 <pacebc522@gmail.com>
2025-08-09 19:31:36 -0600

    final final

--- final/scripts/index.js
+++ final/scripts/index.js
@@ -122,4 +122,12 @@ clearDataBtn.addEventListener("click", () => (
     cleardata()
 ));
 
+const nav = document.querySelector('nav');
+const hamburger_button = document.querySelector(".hamburger");
+
+hamburger_button.addEventListener('click', () => {
+    nav.toggleAttribute("open");
+    hamburger_button.toggleAttribute("open");
+})
+
 fetchData(333);
